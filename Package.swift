e37060fc3d2b16adc7b855b927c5bba8747a5c26
conqueror <f.nayebi@gmail.com>
2018-04-13 17:30:25 -0400

    Curly is removed from the project and is added as dependency

--- Package.swift
+++ Package.swift
@@ -1,22 +1,26 @@
 // swift-tools-version:4.1
 // Generated automatically by Perfect Assistant 2
 // Date: 2018-04-10 12:36:26 +0000
+
 import PackageDescription
 
 let package = Package(
-	name: "PerfectTemplate",
-	dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"4.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", "3.1.0"..<"4.0.0"),
-		.package(url: "https://github.com/iamjono/SwiftMoment.git", "0.0.1"..<"4.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", "3.0.0"..<"4.0.0")
-	],
-	targets: [
-		.target(name: "Lib", dependencies: ["PerfectMySQL", 
-						    "PerfectHTTPServer", 
-						    "SwiftMoment", 
-						    "PerfectRequestLogger"]),
-		.target(name: "App", dependencies: ["Lib"]),
-		.testTarget(name: "LibTests", dependencies: ["Lib"])
-	]
+    name: "PerfectTemplate",
+    dependencies: [
+        .package(url: "https://github.com/PerfectAccelerators/curly.git", from: "0.1.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"4.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", "3.1.0"..<"4.0.0"),
+        .package(url: "https://github.com/iamjono/SwiftMoment.git", "0.0.1"..<"4.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", "3.0.0"..<"4.0.0")
+    ],
+    targets: [
+        .target(name: "Lib", dependencies: [
+            "Curly",
+            "PerfectMySQL",
+            "PerfectHTTPServer",
+            "SwiftMoment",
+            "PerfectRequestLogger"]),
+        .target(name: "App", dependencies: ["Lib"]),
+        .testTarget(name: "LibTests", dependencies: ["Lib"])
+    ]
 )
